c50571bc50f913d13b40b7cc4cad0410f4e52e1d
Cesar <cesar.a.fior@hotmail.com>
2023-10-09 20:09:29 -0300

    2

--- second.py
+++ second.py
@@ -3,15 +3,19 @@
 Document my program
 Dependendo da linguagem do ambeinte o programa vai executar
 ex se nao tiver configurada 
-export LANG=pt_BR
+export LANG=pt_BR.utf8
 """
 __version__ = "0.0.1"
 __author__ = "Cesar Fior"
 __licence__ = "Unlicense"
-current_language = "en_US"
+
+import os
+current_language = os.getenv("LANG")[:5]
 msg = "hello"
 
 if current_language == "pt_BR":
     msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
 
 print(msg)
